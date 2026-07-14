5568ab18468a76c69dfd899691bda6f5004ab334
gas240z <isayev.mustafa.1999@gmail.com>
2026-07-14 11:03:50 +0400

    Add files via upload

---
+++ LTCtask2.py
@@ -0,0 +1,20 @@
+#task1
+# for i in range(1, 11):
+#     print(i)
+
+
+#task2
+# say = 10
+# while say > 0:
+#     print(say)
+#     say-=1
+
+
+#task3
+# metnler = ["Salam", "", "Necəsən", "", "", "Sağ ol"]
+# for metn in metnler:
+#     if metn == "":
+#         continue
+#     print(metn, end=" ")
+
+
