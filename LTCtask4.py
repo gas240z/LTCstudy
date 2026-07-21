55b3ab49f3b3b4cc57e42365ce0309104642d5d2
gas240z <isayev.mustafa.1999@gmail.com>
2026-07-21 12:14:10 +0400

    list

---
+++ LTCtask4.py
@@ -0,0 +1,14 @@
+# fruits = ["alma", "armud", "üzüm", "gilas", "ərik", "şəftəli"]
+# print(len(fruits))
+# print(fruits[2])
+# print(fruits[:3])
+# fruits.append("limon")
+# print(fruits)
+# fruits_upper = [fruit.upper() for fruit in fruits]
+# print(fruits_upper)
+#fruits.insert(0, "limon")  # append en axra qoyur
+
+
+# coordinates = (40.4093, 49.8671)
+# print("Enlik:", coordinates[0])
+# print("Uzunluq:", coordinates[1])
